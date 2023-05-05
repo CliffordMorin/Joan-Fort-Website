acfc832886b1084777311d2b7fda0094c1029fbc
Clifford Morin <cemorin21@gmail.com>
2023-05-05 16:57:32 -0700

    small styling changes to footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@mui/styles";
 export default makeStyles((theme) => ({
   link: {
     textDecoration: "none",
-    color: "#ffffff",
+    color: "theme.palette.text.dark",
     "&:hover": {
       textDecoration: "underline",
     },
